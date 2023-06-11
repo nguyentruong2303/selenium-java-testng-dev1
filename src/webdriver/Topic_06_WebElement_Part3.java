dbfa8e31df863bd448b665c79762ad8b5546c730
nguyentruong2303 <truong922946@gmail.com>
2023-06-11 15:46:45 +0700

    Update Topic 6 and Add Topic 7

--- src/webdriver/Topic_06_WebElement_Part3.java
+++ src/webdriver/Topic_06_WebElement_Part3.java
@@ -2,22 +2,32 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_06_WebElement_Part3 {
 	WebDriver driver;
+	Random rand;
 	String projectPath = System.getProperty("user.dir");
+	String emailAddress;
+
+	By emailTextBox = By.xpath("//input[@id=\"email\"]");
+
+	By passwordTextBox = By.xpath("//input[@id=\"pass\"]");
 
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 		driver = new FirefoxDriver();
+		rand = new Random();
+		emailAddress = "automation" + rand.nextInt(9999) + "@gmail.com";
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -43,14 +53,56 @@ public class Topic_06_WebElement_Part3 {
 	}
 
 	@Test
-	public void TC_02() {
-	
+	public void TC_02_LoginWithInvalidEmail() {
+		driver.get("http://live.techpanda.org/");
+
+		driver.findElement(By.xpath("//div[@class=\"footer\"]//a[@title=\"My Account\"]")).click();
+		sleepInSecond(3);
+
+		driver.findElement(emailTextBox).sendKeys("123456789@1234");
+		driver.findElement(passwordTextBox).sendKeys("12345678");
+
+		driver.findElement(By.xpath("//button[@id=\"send2\"]")).click();
+		sleepInSecond(3);
+
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class=\"validation-advice\"]")).getText(),"Please enter a valid email address. For example johndoe@domain.com.");
+
 	}
 
 	@Test
-	public void TC_03() {
-		
+	public void TC_03_Login_With_Password_LessThan_6_Characters() {
+		driver.get("http://live.techpanda.org/");
+
+		driver.findElement(By.xpath("//div[@class=\"footer\"]//a[@title=\"My Account\"]")).click();
+		sleepInSecond(3);
+
+		driver.findElement(emailTextBox).sendKeys("automation@gmail.com");
+		driver.findElement(passwordTextBox).sendKeys("123");
+
+		driver.findElement(By.xpath("//button[@id=\"send2\"]")).click();
+		sleepInSecond(3);
+
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class=\"validation-advice\"]")).getText(),"Please enter 6 or more characters without leading or trailing spaces.");
+
+
 	}
+	@Test
+	public void TC_04_Login_with_incorrect_Email_Password() {
+		driver.get("http://live.techpanda.org/");
+
+		driver.findElement(By.xpath("//div[@class=\"footer\"]//a[@title=\"My Account\"]")).click();
+		sleepInSecond(3);
+
+		driver.findElement(emailTextBox).sendKeys(emailAddress);
+		driver.findElement(passwordTextBox).sendKeys("123123123");
+
+		driver.findElement(By.xpath("//button[@id=\"send2\"]")).click();
+		sleepInSecond(3);
+
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class=\"error-msg\"]//span")).getText(),"Invalid login or password.");
+	}
+
+
 
 	public void sleepInSecond(long timeInSecond) {
 		try {
---
+++ src/webdriver/Topic_07_Textbox_Textarea.java
@@ -0,0 +1,93 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_07_Textbox_Textarea {
+
+	Random rand;
+
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String emailAddress, firstName, lastName, fullName, password;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		driver = new FirefoxDriver();
+		rand = new Random();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		emailAddress = "automation" + rand.nextInt(9999) + "@gmail.com";
+		firstName = "Automation";
+		lastName = "FC";
+		fullName = firstName + " " + lastName;
+		password = "12345678";
+	}
+
+	@Test
+	public void TC_01_Create_Account() {
+		driver.get("http://live.techpanda.org/");
+
+		driver.findElement(By.xpath("//div[@class=\"footer\"]//a[@title=\"My Account\"]")).click();
+		sleepInSecond(3);
+
+		driver.findElement(By.xpath("//div[@class=\"buttons-set\"]//a[@title=\"Create an Account\"]")).click();
+		sleepInSecond(3);
+
+		driver.findElement(By.xpath("//input[@id=\"firstname\"]")).sendKeys(firstName);
+		driver.findElement(By.xpath("//input[@id=\"lastname\"]")).sendKeys(lastName);
+		driver.findElement(By.xpath("//input[@id=\"email_address\"]")).sendKeys(emailAddress);
+		driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@id=\"confirmation\"]")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@title=\"Register\"]")).click();
+		sleepInSecond(5);
+
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class=\"success-msg\"]//span")).getText(),"Thank you for registering with Main Website Store.");
+		String contactInformation = driver.findElement(By.xpath("//h3[text()=\"Contact Information\"]/parent::div[@class=\"box-title\"]/following-sibling::div[@class=\"box-content\"]/p")).getText();
+		System.out.println(contactInformation);
+
+		Assert.assertTrue(contactInformation.contains(fullName));
+		Assert.assertTrue(contactInformation.contains(emailAddress));
+
+		driver.findElement(By.xpath("//a[@class=\"skip-link skip-account skip-active\"]//span[@class=\"icon\"]")).click();
+		driver.findElement(By.xpath("//a[@title=\"Log Out\"]")).click();
+
+		Assert.assertTrue(driver.findElement(By.xpath("//img[@src=\"http://live.techpanda.org/media/wysiwyg/test/logo.png\"]")).isDisplayed());
+
+
+
+	}
+
+	@Test
+	public void TC_02() {
+	
+	}
+
+	@Test
+	public void TC_03() {
+		
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
