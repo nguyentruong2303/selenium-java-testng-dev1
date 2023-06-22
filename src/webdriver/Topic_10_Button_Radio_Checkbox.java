233c1292067c80259bd3834ea1b3d5fdd8827efa
truong <truong922946@gmail.com>
2023-06-22 17:23:57 +0700

    Add Topic 10 Button_Radio_Checkbox

---
+++ src/webdriver/Topic_10_Button_Radio_Checkbox.java
@@ -0,0 +1,69 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_10_Button_Radio_Checkbox {
+
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		} else {
+			System.setProperty("webdriver.gecko.driver",projectPath + "/browserDrivers/geckodriver.exe");
+		}
+
+		driver = new FirefoxDriver();
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01() {
+		driver.get("https://www.facebook.com/");
+
+		//driver.findElement(By.cssSelector()).isEnabled()
+
+	}
+
+	@Test
+	public void TC_02() {
+	
+	}
+
+	@Test
+	public void TC_03() {
+		
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public int getRandom(int number) {
+		Random rand = new Random();
+	 return	rand.nextInt(9999);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
